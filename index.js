4f205535318542230a93595eac173586fde29014
jperezes <jperezes@cisco.com>
2018-11-08 18:28:10 +0100

    change index.js to use async methods

--- index.js
+++ index.js
@@ -1,348 +1,259 @@
-var colors = require('colors')
-var util = require('util')
-var extend = util._extend;
-var express = require('express')
-var clone = require('clone');
-var async = require('async');
-var bodyParser = require("body-parser");
-var https = require('https');
-var EventEmitter = require('events').EventEmitter;
-var callbackListener = 'webhooklistener'
+let express = require('express')
+let router = express.Router();
+const bodyParser = require("body-parser");
+let rp = require('request-promise');
+const EventEmitter = require('events');
+class SparkBotEmitter extends EventEmitter {}
 
-var SparkBot = function(token, port, botdomain, portInWebook) {
-    EventEmitter.call(this)
-    console.log('Constructing SparkBot '.red);
-    this.app = express();
-    this.token = token;
-    this.port = port;
-    this.portInWebook = portInWebook;
-    this.botdomain = botdomain;
-    this.botname = null;
-    this.options = {
-        host: 'api.ciscospark.com',
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Authorization': 'Bearer ' + token
-        }
-    };
-
-    this.app.get('env')
-    this.app.use(bodyParser.urlencoded({
-        extended: false
-    }));
-    this.app.use(bodyParser.json());
-    this.app.use(express.static('img'))
-    this.app.get('/', function(req, res) {
-        res.send('BOT Initilized')
-    })
-
-    this.app.post('/' + callbackListener, function(req, res) {
-        console.log(('Webhooks API called" ' + JSON.stringify(req.body)).red);
-
-        if (req.body && req.body.resource == 'messages') {
-            var message = req.body.data;
-
-            this.getBotName(function(botname) {
-                console.log('received botname: ' + botname);
-
-                if (message.personEmail != botname) {
-                    this.readMessage(message, function(result) {
-                        var txt = result.text;
-                        var personId = message.personId;
-
-                        this.readPersonDetails(personId, function(personDetails) {
-                            message['message'] = txt;
-                            message['person'] = personDetails;
-                            this.emit('message', message);
-                        }.bind(this));
-                    }.bind(this))
-                }
-            }.bind(this))
-        } else if (req.body && req.body.resource == 'rooms') {
-            var message = req.body.data;
-            this.emit('rooms', message);
-        } else if (req.body && req.body.resource == 'memberships') {
-            var message = req.body.data;
-            this.emit('memberships', message);
-        } else {
-            var message = req.body.data;
-            this.emit('others', message);
-        }
-
-        res.send('')
-    }.bind(this))
-
-    var server = this.app.listen(port, function() {
-        console.log(('Bot started at port: ' + port).red)
-    })
-
-    this.app.get('/init', function(req, res) {
-        this.initializeWeebHooks();
-        res.send('bot re-initialized')
-    }.bind(this))
-
-    this.getOptions = function() {
-        return this.options;
-    }
-
-    this.getBotDomain = function() {
-        return this.botdomain;
-    }
-
-    this.hasPortInwebHook = function() {
-        return this.portInWebook;
-    }
-
-    this.getBotName = function(callback) {
-        if (this.botname == undefined || this.botname == null) {
-            this.getMyDetails(function(result) {
-                this.botname = result['emails'][0];
-                console.log('Botname found: ' + this.botname);
-                callback(this.botname)
-            });
-        } else {
-            callback(this.botname)
-        }
-    }
-
-    this.getPort = function()
-    {
-        return port;
-    }
-}
-
-util.inherits(SparkBot, EventEmitter)
-
-SparkBot.prototype.getServer= function() {
-    return this.app;
+// Default options
+const defaults = {
+  url: "https://api.ciscospark.com",
+  headers:{
+    'Content-Type': 'application/json; charset=utf-8'
+  },
+  json: true,
+  method:'POST',
+	timeout : 5000
 };
 
-
-SparkBot.prototype.init = function() {
-    console.log('Init From SparkBot'.red);
+// Send an API request
+const sendRequest = async (data, parentMethod) => {
+  console.log("SPARK - API Sending object: " + JSON.stringify(data))
+  try {
+    const response  = await rp(data);
+    return response;
+  } catch (e) {
+    console.log("[ "+ parentMethod +" ]: " +"Error sending the Request to Spark API: " + JSON.stringify(e))
+    throw e
+  }
 };
 
-SparkBot.prototype.printHelloWorld = function() {
-    console.log('Hello World From SparkBot'.red);
-};
+/**
+ * SparkBotApi register the bot to Cisco Spark Api
+ * @param {String}        token          Bot token Key
+ * @param {Number}        port           Port listening for events
+ * @param {String}        botdomain      Bot url listening to events
+ * @param {Number}        webhookPort    Not used yet, for websockets
+ */
+class SparkBotApi {
+	constructor(token, port, botdomain,webhookPort) {
+    Object.assign(defaults.headers,{'Authorization': 'Bearer ' + token})
+		this.config = Object.assign({port, botdomain}, defaults);
+    this.sparkBotEmitter = new SparkBotEmitter();
+    this.app = express()
+    this.initServer(this.app);
+    this.initializeWeebHooks();
+	}
 
-SparkBot.prototype.initializeWeebHooks = function() {
-    this.deleteAllWebHooks(function() {
-        console.log("All registered callbacks deleted");
-        this.registerWebHooks()
-    }.bind(this));
-}
-
-SparkBot.prototype.deleteAllWebHooks = function(doneCallback) {
-    var regWebHooks;
-    this.readRegisteredWebHooks(function(result) {
-        regWebHooks = result;
-        if(JSON.stringify(regWebHooks) === '{}')
-        {
-            doneCallback();
-        }else
-        {
-          this.deleteWebHooks(regWebHooks.items, function() {
-              doneCallback();
-          })
-        }
-    }.bind(this));
-}
-
-SparkBot.prototype.deleteWebHooks = function(webHooks, doneCallback) {
-
-    var queue = async.queue(function(webhook, callback) {
-        this.deleteWebHook(webhook, function() {
-            callback();
-        });
-    }.bind(this), 5);
-
-    queue.drain = function() {
-        doneCallback()
+  /**
+   * This method register the webhook for all events on the Spark Api
+   */
+  async registerWebHooks(){
+    console.log("SPARK API - about to register webhooks");
+    let options = Object.assign({},this.config);
+    options.url = options.url + "/v1/webhooks"
+    const callbackListener = 'v1/webhooklistener'
+    const targetUrl = options.botdomain + '/' + callbackListener;
+    const messageData = {
+         'name': 'GlobalListener',
+         'targetUrl':targetUrl,
+         'resource': 'messages',
+         'event': 'all'
     }
+    let data = Object.assign( options, {body:messageData});
+    const response = await sendRequest(data, 'registerWebHooks');
+    return response;
+  }
 
-    queue.push(webHooks, function(err) {});
-}
+  /**
+   * This method returns all registered webhooks on the Spark API
+   */
+  async readRegisteredWebHooks() {
+      let options = Object.assign({},defaults);
+      options.url = options.url + '/v1/webhooks'
+      options.method = 'GET'
+      const result = await sendRequest(options, 'readRegisteredWebHooks')
+      return result
+  }
 
-SparkBot.prototype.deleteWebHook = function(webHook, doneCallback) {
-    console.log('deleteWebHook' + JSON.stringify(webHook));
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/webhooks/" + webHook.id
-    });
-    extendedOptions['method'] = 'DELETE'
-    SparkBot.sendRequest(extendedOptions, doneCallback);
-}
+  /**
+   * This method unregister the specified webhooks
+   * @param {object}   webHook webhook to be unregistered
+   */
+  async deleteWebHook (webHook) {
+      let options = Object.assign({},defaults);
+      options.url = options.url + '/v1/webhooks/'  + webHook.id
+      options.method = 'DELETE'
+      const result = await sendRequest(options,'deleteWebHook');
+  }
 
-SparkBot.prototype.registerWebHooks = function() {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/webhooks"
-    });
-
-    var targetUrl = 'http://' + this.getBotDomain() +( isNaN(this.getPort()) ? '' : this.getPort() ) +'/' + callbackListener;
-
-    if(!this.hasPortInwebHook())
-    {
-        targetUrl = this.getBotDomain() +'/' + callbackListener;
+  /**
+   * This method unregister All registred webhooks for the Bot
+   */
+  async deleteAllWebHooks() {
+    try {
+      let webhooks = await this.readRegisteredWebHooks()
+      if(typeof(webhooks.items[0].id) === 'undefined') {
+        console.log("no webhooks registered")
+      } else {
+        webhooks.items.forEach(async (item) => {
+            if(typeof(item) === 'undefined') {
+              return
+            }
+            await this.deleteWebHook(item);
+        })
+      }
+    } catch(e) {
+        console.log("error deleting the webhook: " + e)
     }
+    return false;
+  }
 
-    var messageData = {
-        'name': 'GlobalListener',
-        'targetUrl': targetUrl,
-        'resource': 'messages',
-        'event': 'all'
+  /**
+   * This method initializer the webhooks for the Bot by deleting all and registering again
+   */
+  async initializeWeebHooks() {
+    try {
+      await this.deleteAllWebHooks();
+      console.log("All registered callbacks deleted");
+      await this.registerWebHooks();
+    } catch (e) {
+      console.log("error initializing the webhooks: " + e)
     }
-    extendedOptions['method'] = 'POST';
+  }
 
-    SparkBot.sendRequestWithData(messageData, extendedOptions, function() {
-        console.log('request sent - registerWebHooks');
-    })
-}
+  async getMyDetails() {
+    let options = Object.assign({},defaults);
+    options.url = defaults.url + '/v1/people/me'
+    options.method = 'GET'
+    const result = await sendRequest(options, 'getMyDetails');
+    return result;
+  }
 
-SparkBot.prototype.getMyDetails = function(callback) {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/people/me"
-    });
+  async readMessage(message) {
+    let options = Object.assign({},defaults);
+    options.url = options.url + '/v1/messages/' + message.id
+    options.method = 'GET'
+    const result = await sendRequest(options,'readMessage');
+    return result;
+  }
 
-    SparkBot.sendRequest(extendedOptions, function(result) {
-        console.log('request sent - getMyDetails');
-        callback(result);
-    })
-}
+  async readPersonDetails(personId) {
+    let options = Object.assign({},defaults);
+    options.url = options.url + '/v1/people/' + personId
+    options.method = 'GET'
+    const result = await sendRequest(options, 'readPersonDetails');
+    return result
+  }
 
-SparkBot.prototype.sendMessage = function(roomId, txt, callback) {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/messages/"
-    });
-    extendedOptions['method'] = 'POST';
-    var messageData = {
-        'roomId': roomId,
-        'text': txt
-    }
-
-    SparkBot.sendRequestWithData(messageData, extendedOptions, function(result) {
-        callback(result);
-    })
-}
-
-SparkBot.prototype.sendRichTextMessage = function(roomId, txt, callback) {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/messages/"
-    });
-    extendedOptions['method'] = 'POST';
-    var messageData = {
-        'roomId': roomId,
-        'markdown': txt
-    }
-    SparkBot.sendRequestWithData(messageData, extendedOptions, function(result) {
-        callback(result);
-    })
-}
-
-SparkBot.prototype.sendRichTextMessageToDirectPerson = function(personEmail, txt, callback) {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/messages/"
-    });
-    extendedOptions['method'] = 'POST';
-    var messageData = {
-        'toPersonEmail': personEmail,
-        'markdown': txt
-    }
-    SparkBot.sendRequestWithData(messageData, extendedOptions, function(result) {
-        callback(result);
-    })
-}
-
-SparkBot.prototype.sendMessageWithFile = function(roomId, txt, filesUrl, callback) {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/messages/"
-    });
-    extendedOptions['method'] = 'POST';
-    var messageData = {
-        'roomId': roomId,
-        'text': txt,
-        'files': filesUrl
-    }
-
-    SparkBot.sendRequestWithData(messageData, extendedOptions, function(result) {
-        callback(result);
-    })
-}
-
-SparkBot.prototype.readMessage = function(message, callback) {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/messages/" + message.id
-    });
-
-    SparkBot.sendRequest(extendedOptions, function(result) {
-        console.log('request sent - readMessage');
-        callback(result);
-    })
-}
-
-SparkBot.prototype.readPersonDetails = function(personId, callback) {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/people/" + personId
-    });
-
-    SparkBot.sendRequest(extendedOptions, function(result) {
-        console.log('request sent - readPersonDetails');
-        callback(result);
-    })
-}
-
-SparkBot.prototype.readRegisteredWebHooks = function(callback) {
-    var optionsCloned = clone(this.getOptions());
-    var extendedOptions = extend(optionsCloned, {
-        path: "/v1/webhooks"
-    });
-
-    SparkBot.sendRequest(extendedOptions, function(result) {
-        console.log('request sent - readRegisteredWebHooks');
-        callback(result);
-    })
-}
-
-SparkBot.sendRequest = function(argOptions, callback) {
-    SparkBot.sendRequestWithData(null, argOptions, callback);
-}
-
-SparkBot.sendRequestWithData = function(data, argOptions, callback) {
-    var req = https.request(argOptions, function(res) {
-        var bodyChunks = [];
-        res.setEncoding('utf8');
-        res.on('data', function(chunk) {
-            bodyChunks.push(chunk);
-        });
-
-        res.on('end', function() {
-            var result = {};
-
-            try {
-                result = JSON.parse(bodyChunks);
-            } catch (e) {}
-            callback(result);
-        });
-    });
-
-    req.on('error', function(err) {
-        console.log(err);
-    });
-
-    if (data) {
-        req.end(JSON.stringify(data));
+  async getBotName() {
+    if (this.botname == undefined || this.botname == null) {
+      try {
+        const result = await this.getMyDetails()
+        this.botName = result.emails[0]
+        console.log("Bot name found: " + this.botName);
+      }catch(e){
+        console.log("Error getting my details" + e)
+      }
     } else {
-        req.end();
+        return this.botName;
     }
+  }
+
+  async sendMessage(roomId, txt) {
+    let options = Object.assign({},defaults);
+    options.url = options.url + "/v1/messages/"
+      var messageData = {
+          'roomId': roomId,
+          'text': txt
+      }
+      Object.assign(options,{body:messageData})
+      const result = await sendRequest(options, 'sendMessage');
+  }
+  async sendMessageToDirectPerson(personEmail, txt) {
+      let options = Object.assign({},defaults);
+      options.url = options.url + "/v1/messages/"
+        var messageData = {
+            'toPersonEmail': personEmail,
+            'text': txt
+        }
+        Object.assign(options,{body:messageData})
+        const result = await sendRequest(options, 'sendMessageToDirectPerson');
+        return result
+  }
+  async sendRichTextMessage(roomId, txt) {
+      let options = Object.assign({},defaults);
+      options.url = options.url + "/v1/messages/"
+      var messageData = {
+          'roomId': roomId,
+          'markdown': txt
+      }
+      Object.assign(options,{body:messageData})
+      const result = await sendRequest(options, 'sendRichTextMessage');
+      return result;
+  }
+
+  async handlePostRequest(req) {
+    try {
+      if (typeof(req.body) === 'undefined') {
+        return "error parsing the body";
+      }
+      if (req.body.resource == 'messages') {
+          console.log("POST event received:\n" + req)
+          var message = req.body.data;
+          await this.getBotName();
+          if (message.personEmail !== this.botName) {
+            const result = await this.readMessage(message);
+            let txt = result.text;
+            let personId = message.personId;
+            const personDetails = await this.readPersonDetails(personId);
+            message.message = txt
+            message.person = personDetails
+            this.sparkBotEmitter.emit('message',message)
+          }
+          return "Webhook received"
+      }
+    } catch(e) {
+      console.log("error handling the post request: " + e)
+      throw e
+    }
+  }
+
+  /**
+   * Init the express server
+   */
+  initServer(app){
+    console.log("Spark Bot API - about to init the server")
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use('/v1',router);
+    let port = this.config.port
+    router.use(function (req, res, next) {
+      next();
+    });
+    router.route('/webhooklistener').post(async (req, res) => {
+      try {
+        console.log("webhook received")
+        let result = await this.handlePostRequest(req)
+        res.send(result)
+      } catch(e) {
+        res.send("Error processing the post request" + e)
+      }
+
+    });
+    router.route('/init').get(async(req, res) =>  {
+      try{
+        await this.initializeWeebHooks();
+        res.send('bot re-initialized')
+      } catch(e) {
+        res.send("error initializing");
+      }
+
+    });
+    app.listen(port, function(port) {
+        console.log(('\n\nBot started at port: ' + port).red)
+    })
+  }
 }
 
-module.exports.SparkBot = SparkBot;
+module.exports = SparkBotApi;
